acd621c267d7461be42baed6915cc6b6976a8aaa
arya <arya@aryaputramaheswara34@gmail.com>
2023-07-12 18:55:12 +0700

    menggunakan absolute path untuk copy dan kirim email

--- cek-server.sh
+++ cek-server.sh
@@ -1,14 +1,16 @@
 #untuk cek apakah website pnj down atau tidak
 #kalo /dev/null aja error nya bakal muncul ke terminal. Pake 2>&1 untuk silence semua output
-ping elearning.pnj.ac.id -c 1 > /dev/null 2>&1
+#ping elearning.pnj.ac.idd -c 5 > /dev/null 2>&1
 
 #masukkan hasil exit code ke variabel bool_ping
+ping elearning.pnj.ac.id -c 1 > /dev/null 2>&1
 bool_ping=$?
 
 #untuk cek apakah nginx nya running atau tidak
-sudo systemctl status nginx | grep "Active: active" > /dev/null 2>&1
+#sudo systemctl status nginx | grep "Active: active" > /dev/null 2>&1
 
 #masukkan hasil exit code ke variabel bool_nginx
+sudo systemctl status nginx | grep "Active: active" > /dev/null 2>&1
 bool_nginx=$?
 
 #variabel isi pesan ataupun subjek
@@ -23,7 +25,7 @@ email="/home/arya/shell_uas/email1.py"
 template="/home/arya/shell_uas/template_email.py"
 
 #copy isi template email
-cp template_email.py email1.py
+cp $template $email
 
 kirim_email()
 {
@@ -33,7 +35,7 @@ kirim_email()
 #bisa ping dan nginx
 if [ $bool_ping -eq 0 ] && [ $bool_nginx -eq 0 ];then
 
-    echo $suskses
+    echo "$suskses"
     sed -i "s/isi_pesan/$sukses/" $email
     sed -i "s/isi_subjek/$subjek/" $email
     kirim_email
@@ -41,7 +43,7 @@ if [ $bool_ping -eq 0 ] && [ $bool_nginx -eq 0 ];then
 #hanya nginx
 elif [ $bool_nginx -eq 0 ];then
 
-    echo berhasil nginx gagal ping
+    echo "$nginx"
     sed -i "s/isi_pesan/$nginx/" $email
     sed -i "s/isi_subjek/$subjek/" $email
     kirim_email
@@ -49,14 +51,14 @@ elif [ $bool_nginx -eq 0 ];then
 #hanya ping
 elif [ $bool_ping -eq 0 ];then
 
-    echo berhasil ping gagal nginx
+    echo "$ping"
     sed -i "s/isi_pesan/$ping/" $email
     sed -i "s/isi_subjek/$subjek/" $email
     kirim_email
 
 else
 #tidak bisa ping dan nginx
-    echo server mati
+    echo "$down"
     sed -i "s/isi_pesan/$down/" $email
     sed -i "s/isi_subjek/$subjek/" $email
     kirim_email
