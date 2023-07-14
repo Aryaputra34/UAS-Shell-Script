8c25881099d894d36a1005dd4470007de27450df
Joko Prasetyo <92915640+7oxP@users.noreply.github.com>
2023-07-14 15:37:01 +0700

    Insert log ke dalam database

---
+++ db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+from datetime import datetime
+
+def getMysqlConnection():
+    return mysql.connector.connect(host="localhost", user="root", password="", database="server")
+
+curr_time = datetime.now()
+formatted_time = curr_time.strftime('%Y-%m-%d %H:%M:%S')
+print(formatted_time)
+
+db = getMysqlConnection()
+cur = db.cursor()
+query = f"INSERT INTO server_log VALUES(NULL,'server mati, tidak bisa ping ke website dan web server down','{formatted_time}')"
+cur.execute(query)
+db.commit()
